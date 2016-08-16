c37b8ec3bc43f9c17c742d024fd2305177a8aef1
khamla719 <khamla719@yahoo.com>
2016-08-16 18:38:21 -0400

    Add letterGrade function to Student object

--- part-1/student.js
+++ part-1/student.js
@@ -10,3 +10,22 @@ Student.prototype.averageScore = function() {
   }
   return Math.floor(total / this.scores.length);
 }
+
+Student.prototype.letterGrade = function() {
+  var grade = this.averageScore();
+  if (grade >= 90) {
+    return "A";
+  }
+  else if (grade >= 80 && grade < 90) {
+    return "B";
+  }
+  else if (grade >= 70 && grade < 80) {
+    return "C";
+  }
+  else if (grade >= 60 && grade < 70) {
+    return "D";
+  }
+  else {
+    return "F";
+  }
+}
